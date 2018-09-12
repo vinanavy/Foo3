f0bf96b6e6d9da3c387107590f350e2ed06400c1
QuocDatDev <vinanavy@gmail.com>
2018-09-12 20:40:38 +0700

    Added Files

---
+++ Link.cs
@@ -0,0 +1,40 @@
+OOP
+https://nguyenanhtuanweb.wordpress.com/2018/03/16/tong-quan-ve-lap-trinh-huong-doi-tuong/
+https://www.mastercode.vn/blog/web-development/bai-4-classes-objects-va-namespaces-nen-tang-lap-trinh-c.52
+http://timoday.edu.vn/phan-3-lap-trinh-huong-doi-tuong-trong-c/
+https://tedu.com.vn/lap-trinh-c/top-10-khai-niem-trong-oop-voi-cnet-50.html
+
+https://techblog.vn/nhung-tinh-nang-moi-trong-c-70
+https://www.youtube.com/watch?v=UI2RRcsR83s&feature=youtu.be (C# tutorial for beginners to Advance)
+
+https://giphy.com/gifs/joker-the-dark-knight-heath-ledger-ZQ5YmzRHjUyUo (gif for joker)
+https://medium.com/front-end-hacking/callbacks-promises-and-async-await-ad4756e01d90
+https://blog.risingstack.com/mastering-async-await-in-nodejs/
+https://sacombankcareer.com/tin-tuc/tra-loi-cau-hoi-muc-tieu-5-nam-toi-cua-ban-la-gi-.35a52a65/vi
+https://techkids.vn/blog/cac-cau-lenh-git-khong-khong-biet/
+http://rogerdudler.github.io/git-guide/index.vi.html
+https://www.youtube.com/watch?v=VI07ouVS5FE&index=4&list=PLhW3qG5bs-L8OlICbNX9u4MZ3rAt5c5GG (GIT)
+https://backlog.com/git-tutorial/vn/intro/intro1_1.html
+
+//Important
+https://dthshare.com/hoi-gi-ve-huong-doi-tuong-oop-khi-di-phong-van/
+http://minhhn.com/lap-trinh-c-sharp/overload-trong-csharp/
+
+http://code5s.com/windows/visual-c-sharp/tu-khoa-base-va-new-trong-csharp.html
+http://minhhn.com/lap-trinh-c-sharp/lop-truu-tuong-abstract-class-trong-csharp/
+https://yinyangit.wordpress.com/2012/01/15/oop-interface-vs-abstract-class/
+
+http://www.iceteaviet.com/2017/03/khi-nao-nen-su-dung-tu-khoa-override.html
+https://www.youtube.com/results?search_query=git+with+visual+studio+2017+
+https://www.youtube.com/watch?v=c3482qAzZLQ
+https://docs.microsoft.com/en-us/azure/devops/repos/git/gitquickstart?view=vsts&tabs=visual-studio
+http://trashvin.blogspot.com/2017/05/integrating-github-on-visual-studio_5.html
+
+
+
+
+
+https://www.youtube.com/channel/UCCZBmCKPpdIzOSfXoSmisDg (Học Tiếng Anh Online Miễn Phí)
+https://www.youtube.com/channel/UCgYiqFZll4CeKqxtDum54Og?sub_confirmation=1 (Trung Tâm Tiếng Anh Jaxtina)
+https://efc.edu.vn/
+
